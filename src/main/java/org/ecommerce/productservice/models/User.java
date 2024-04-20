c73f7eb36aff43818d77528266c3cc8d4222c3b9
saiteja154 <rapellisaiteja154@gmail.com>
2024-04-20 17:59:14 +0530

    created a User table and Established the Connection with Mysql DB

---
+++ src/main/java/org/ecommerce/productservice/models/User.java
@@ -0,0 +1,20 @@
+package org.ecommerce.productservice.models;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "ecomm_name")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(name="email_address" ,unique = true)
+    private String email;
+
+}
